1120770ec77a26fa7d99a4a78dd1a5dcf26e53f8
Pashmi <lowneonema@gmail.com>
2019-10-06 22:01:16 +0200

    Boostrap wip

--- script.js
+++ script.js
@@ -32,13 +32,17 @@ const getData = () => {
         if (request.readyState === 4 && request.status === 200)
         {
             const insertPlace = document.getElementById('tdl-content');
+            //Delete the previous data to add every new ones
             while (insertPlace.hasChildNodes())
                     insertPlace.removeChild(insertPlace.childNodes[0]);
             console.log(request.response);
 
+
             const t = JSON.parse(request.response);
             t.forEach((x) =>
             {
+                const div = document.createElement("DIV");
+                div.className = "row mt-lg-1 mt-sm-3 mt-xl-2 mt-xs-3";
                 let task =  document.createElement('P');
                 let btn = document.createElement("BUTTON");
                 let checkbox = document.createElement("INPUT");
@@ -47,7 +51,10 @@ const getData = () => {
                 checkbox.value="value";
                 checkbox.checked = x.isdone;
                 checkbox.id="id";
-                insertPlace.appendChild(checkbox);
+
+                checkbox.className = "col-lg-1 styled col-sm-2";
+                insertPlace.appendChild(div);
+                div.appendChild(checkbox);
 
                 checkbox.addEventListener('input', () => {
                     const request = new XMLHttpRequest();
@@ -62,7 +69,8 @@ const getData = () => {
                     };
                 });
                 btn.innerHTML = "Remove";
-                insertPlace.appendChild(btn);
+                btn.className = "col-lg-1 btn-danger col-sm-2";
+
 
 
                 btn.addEventListener('click',() =>{
@@ -76,8 +84,12 @@ const getData = () => {
                     };
                 });
                 task.innerText = x.content;
+                task.className = "col-lg-6 col-sm-10";
+
                 //TODO add some content (checkbox for instance)
-                insertPlace.appendChild(task);
+                div.appendChild(task);
+                div.appendChild(btn);
+
             })
         }
     };
